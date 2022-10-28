b1adeb7fd7bcf8276fbe34b613834b6e3d7f0dd7
Myriam <yria.code@gmail.com>
2022-10-28 13:40:46 +0200

    ajout du calcul dans le bon fichier et test unitaire pour la réduction de 5%

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -55,6 +55,21 @@ public class FareCalculatorServiceTest {
         fareCalculatorService.calculateFare(ticket);
         assertEquals(0, ticket.getPrice());
     }
+    
+    @Test
+    public void calculateFareCarForReduc5(){
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (  60 * 60 * 1000) );
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setForCurrentUser(true);
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals(Fare.CAR_RATE_PER_HOUR * 0.95, ticket.getPrice());
+    }
 
     @Test
     public void calculateFareBike(){
