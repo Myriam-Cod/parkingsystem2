6cd24e5c14caf52e7fd0643fd80d2118fa273301
Myriam <yria.code@gmail.com>
2022-10-28 13:04:37 +0200

    ajout du calcul de la fonctionnalité 30 free dans le bon fichier pour faire passer le test!

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -18,16 +18,21 @@ public class FareCalculatorService {
         double duration = (outHour - inHour) / 1000;
         duration = duration / (60*60);
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
-            }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+        if(duration < 0.5) {
+            ticket.setPrice(0);
+        }else {
+        	  switch (ticket.getParkingSpot().getParkingType()){
+              case CAR: {
+                  ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                  break;
+              }
+              case BIKE: {
+                  ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                  break;
+              }
+              default: throw new IllegalArgumentException("Unkown Parking Type");
+          }
         }
+      
     }
 }
