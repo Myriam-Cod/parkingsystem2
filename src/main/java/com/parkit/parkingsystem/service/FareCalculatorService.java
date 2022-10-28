36b0024ab7d2a74fec3693a18294ff05c3e34bdd
Myriam <yria.code@gmail.com>
2022-10-28 12:40:35 +0200

    correction du calcul de la durée

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -14,7 +14,8 @@ public class FareCalculatorService {
         long outHour = ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        long duration = outHour - inHour;
+        double duration = (outHour - inHour) / 1000;
+        duration = duration / (60*60);
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
